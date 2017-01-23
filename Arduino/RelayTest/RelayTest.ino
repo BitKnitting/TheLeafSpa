8c2704c39a46030f7ddc7579a80a4f5a336b12d7
FarmerRobbie <farmerrobbie@freshsalad.today>
2017-01-23 02:06:49 -0800

    updated RelayTest.ino

--- Arduino/RelayTest/RelayTest.ino
+++ Arduino/RelayTest/RelayTest.ino
@@ -19,7 +19,7 @@ DHT dht(DHTPIN, DHTTYPE);
 #include <SoftwareSerial.h>
 SoftwareSerial CO2sensor(5, 6);     // TX, RX
 //Identify relay pin
-#define RELAYPIN1 4 //D4 on the Grove Shield
+#define RELAYPIN4 7
 /*
    SETUP
 */
@@ -27,18 +27,18 @@ void setup() {
   DEBUG_BEGIN;
   DEBUG_PRINTF("The amount of available ram: ");
   DEBUG_PRINTLN(freeRam());
-  pinMode(RELAYPIN1, OUTPUT);
+  pinMode(RELAYPIN4, OUTPUT);
 }
 /*
    LOOP
 */
 void loop() {
   //Turn relay ON
-  digitalWrite(RELAYPIN1, LOW);
-  //Wait 10 seconds
-  delay(10000);
+  digitalWrite(RELAYPIN4, LOW);
+  //Wait 
+  delay(3000);
   //Turn relay OFF
-  digitalWrite(RELAYPIN1, HIGH);
-  //Wait 10 seconds
-  delay(10000);
+  digitalWrite(RELAYPIN4, HIGH);
+  //Wait 
+  delay(3000);
 }
